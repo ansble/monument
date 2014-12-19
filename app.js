1b08b3b4b7b7adc99dccaad04ec3689653a37730
Daniel Sellers <daniel@designfrontier.net>
2014-12-19 07:36:28 -0700

    abstracted out the server a bunch... well compartmentalized it a bunch

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
 var routes = require('./routes.json')
-	, emitter = require('./emitter.js')
-	, http = require('http');
+	, http = require('http')
 
-//setup the routes
-require('./routes/');
+	//setup the routes
+	, server = require('./routes/')(http, routes);
 
-http.createServer(function (req, res) {
-
-	if(routes[req.url] && routes[req.url].indexOf(req.method.toLowerCase()) !== -1){
-		emitter.emit('route:' + req.url + ':' + req.method.toLowerCase(), {req: req, res: res});
-	} else {
-		res.writeHead(404, {'Content-Type': 'text/plain'});
-		res.end('Route not found');
-	}
-}).listen(3000, '127.0.0.1');
+server.listen(3000, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:3000/');
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var path = require('path')
 	, normalizedPath = path.join(__dirname)
-	, fs = require('fs');
+	, fs = require('fs')
+	, http = require('http')
+	, emitter = require('../emitter.js')
+
+	, server;
 
 fs.readdirSync(normalizedPath).forEach(function(file) {
   if(file !== 'index.js'){
   	require("./" + file);
   }
 });
+
+//need to recieve the http/https object, the routes.json file
+server = function(serverType, routesJson){
+
+	return serverType.createServer(function (req, res) {
+
+		if(routesJson[req.url] && routesJson[req.url].indexOf(req.method.toLowerCase()) !== -1){
+			emitter.emit('route:' + req.url + ':' + req.method.toLowerCase(), {req: req, res: res});
+		} else {
+			res.writeHead(404, {'Content-Type': 'text/plain'});
+			res.end('Route not found');
+		}
+	});
+}
+
+module.exports = server;
