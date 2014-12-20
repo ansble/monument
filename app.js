0e9dba63a926eb108583af2a82ebc6206ffe737c
Daniel Sellers <daniel@designfrontier.net>
2014-12-20 10:31:23 -0700

    tweaking things so it runs in heroku

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var routes = require('./routes.json')
 	, http = require('http')
+	, port = process.env.PORT || 3000
 
 	//setup the routes and server
 	//	pass in the http or https object and the routes.json
 	//	then listen below on port/address you want to
 	, server = require('./routes/')(http, routes);
 
-server.listen(3000, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:3000/');
+server.listen(port);
+console.log('Server running at http://127.0.0.1:' + port + '/');
