819c6da7255d951111d2d88eeb281b9be20fcaf3
Daniel Sellers <daniel@designfrontier.net>
2014-12-19 22:02:40 -0700

    wrapped up the first shot at getting static routes working

--- routes/index.js
+++ routes/index.js
@@ -4,23 +4,46 @@ var path = require('path')
 	, http = require('http')
 	, emitter = require('../emitter.js')
 
+	, publicFolders = []
+
 	, server;
 
 //load in all the route handlers
-fs.readdirSync(normalizedPath).forEach(function(file) {
+fs.readdirSync(normalizedPath).forEach(function (file) {
 	if(file !== 'index.js'){
 		require("./" + file);
 	}
 });
 
-server = function(serverType, routesJson){
+//load in all the static routes
+fs.readdirSync('./public').forEach(function (file) {
+	if(fs.statSync('./public/' + file).isDirectory()){
+		publicFolders.push(file);
+	}
+});
 
+server = function (serverType, routesJson) {
 	return serverType.createServer(function (req, res) {
-		if(routesJson[req.url] && routesJson[req.url].indexOf(req.method.toLowerCase()) !== -1){
+		//match the first part of the url... for public stuff
+		if (publicFolders.indexOf(req.url.split('/')[1]) !== -1) {
+			//static assets y'all
+			//read in the file and stream it to the client
+			fs.exists('./public' + req.url, function (exists) {
+				if(exists){
+					fs.createReadStream('./public' + req.url).pipe(res);
+				} else {
+					//replace with the error route
+					res.writeHead(404, {'Content-Type': 'text/plain'});
+					res.end('file not found');
+				}
+			});
+		} else if (routesJson[req.url] && routesJson[req.url].indexOf(req.method.toLowerCase()) !== -1) {
+			//matches a route in the routes.json
 			emitter.emit('route:' + req.url + ':' + req.method.toLowerCase(), {req: req, res: res});
 		} else {
+			//todo replace with the error route...
 			res.writeHead(404, {'Content-Type': 'text/plain'});
-			res.end('Route not found');
+			res.end('file not found');
 		}
 	});
 }
