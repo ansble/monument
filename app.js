32362e9100c780e1d6be8b22cc016f71efd62d7e
Daniel Sellers <daniel@designfrontier.net>
2015-01-13 19:37:20 -0700

    working on this thing... cleaning it up and hopefully it works

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 var routes = require('./routes.json')
 	, http = require('http')
-	, port = process.env.PORT || 3000
 
 	//setup the routes and server
 	//	pass in the http or https object and the routes.json
 	//	then listen below on port/address you want to
-	, server = require('./routes/')(http, routes);
+	, server
 
-server.listen(port);
-console.log('Server running at http://127.0.0.1:' + port + '/');
+	, wrapper = function (config) {
+		var port = config.port || 3000;
+
+		server = require(config.routePath)(http, routes);
+
+		server.listen(port);
+
+		console.log('monumentjs running at http://127.0.0.1:' + port + '/');
+	};
+
+module.exports = {server: wrapper};
