e444d9438e22724f2f877561744493a78f218e6d
Daniel Sellers <daniel@designfrontier.net>
2015-01-24 08:01:50 -0700

    seems to be working now. Need to test across the board in lots of browsers... but I believe it is working. #31"

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var routes
 	, http = require('http')
 	, dot = require('dot')
 	, path = require('path')
+	, getRawBody = require('raw-body')
+	, typer = require('media-typer')
+	, querystring = require('querystring')
 
 	, pkg = require('./package.json')
 
@@ -9,6 +12,52 @@ var routes
 	//	pass in the http or https object and the routes.json
 	//	then listen below on port/address you want to
 	, server
+
+	, parseForm = function (formString) {
+		var rtnObj = {}
+			, keys  = formString.match(/([\n\r].+)/g) //formString.match(/(form-data; name=['"])([^"']+)/g)
+			, currenName;
+
+		if(keys !== null){
+			keys.forEach(function (item) {
+				var name = item.match(/(form-data; name=['"])([^"']+)/);
+				if (name) {
+					//this is a key
+					currentName = name[0].replace(/form-data; name=['"]/, '');
+				} else if (!item.match(/FormBoundary/ && typeof currentName !== 'undefined')) {
+					//this is a value
+					rtnObj[currentName] = item.replace(/[\n]/,'');
+				}
+			});
+		} else {
+			rtnObj = querystring.parse(formString);
+		}
+
+		return rtnObj;
+	}
+
+	, parser = function (req, callback, scope) {//parse out the body
+		var busboy;
+
+		getRawBody(req, {
+		    length: req.headers['content-length'],
+		    limit: '1mb',
+		    encoding: typer.parse(req.headers['content-type']).parameters.charset || 'UTF-8'
+		  }, function (err, string) {
+		    if (err){
+		      emitter.emit('error:parse', err);
+		      return err;
+		    }
+		    
+	    	try{
+	    		req.body = JSON.parse(string);
+	    		callback.apply(scope);
+	    	} catch (e) {
+		    	req.body = parseForm(string);
+		    	callback.apply(scope);
+	    	}
+		});
+	}
 
 	, wrapper = function (config) {
 		var port = config.port || 3000
@@ -28,4 +77,8 @@ var routes
 		console.log('monumentjs v' + pkg.version +' up and running on port: ' + port);
 	};
 
-module.exports = {server: wrapper, events: require('./emitter.js')};
+module.exports = {
+	server: wrapper
+	, events: require('./emitter.js')
+	, parser: parser
+};
