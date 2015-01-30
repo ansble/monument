d335d053e635d33a3454f5b2df05516c521a0448
Daniel Sellers <daniel@designfrontier.net>
2015-01-30 16:06:56 -0700

    working away on the send method... almost have it cleaned up and to par

--- utils/send.js
+++ utils/send.js
@@ -1,89 +1,82 @@
 method.exports = function (data) {
-	var type = typeof data;
+	var type = typeof data
+		, isBuffer = Buffer.isBuffer(data);
+
+	//   switch (typeof chunk) {
+	//     // string defaulting to html
+	//     case 'string':
+	//       if (!this.get('Content-Type')) {
+	//         this.type('html'); TODO: What is this.type???
+	//       }
+	//       break;
+	//     case 'boolean':
+	//     case 'number':
+	//     case 'object':
+	//       if (chunk === null) {
+	//         chunk = '';
+	//       } else if (Buffer.isBuffer(chunk)) {
+	//         if (!this.get('Content-Type')) {
+	//           this.type('bin'); TODO: What is this.type???
+	//         }
+	//       } else {
+	//         return this.json(chunk); TODO: What is this.json???
+	//       }
+	//       break;
+	//   }
 
 	if (type === 'string') {
+		//   // write strings in utf-8
+		//   if (typeof chunk === 'string') {
+		//     encoding = 'utf8';
+		//     type = this.get('Content-Type');
+
+		//     // reflect this in content-type
+		//     if (typeof type === 'string') {
+		//       this.set('Content-Type', setCharset(type, 'utf-8')); TODO: What does this do??
+		//     }
+		//   }
 		this.setHeader('Content-Type', 'text/html');
+		this.setEncoding('utf8');
 		this.end(data);
-	} else if (type === 'object') {
-		if (Buffer.isBuffer(data)) {
+	} else if (type === 'object' || type === 'number' || type === 'boolean') {
+		if (isBuffer) {
 			this.setHeader('Content-Type', 'application/octet-stream');
 		} else {
 			this.setHeader('Content-Type', 'application/json');
 		}
 		this.end(data);
 	}
+
+	if (type !== 'undefined') {
+		//   // populate Content-Length
+		//   if (chunk !== undefined) {
+		//     if (!Buffer.isBuffer(chunk)) {
+		//       // convert chunk to Buffer; saves later double conversions
+		//       chunk = new Buffer(chunk, encoding);
+		//       encoding = undefined;
+		//     }
+
+		//     len = chunk.length;
+		//     this.set('Content-Length', len);
+		//   }
+	}
+
+	//   // freshness
+	//   if (req.fresh) this.statusCode = 304;
+
+	//   // strip irrelevant headers
+	//   if (204 == this.statusCode || 304 == this.statusCode) {
+	//     this.removeHeader('Content-Type');
+	//     this.removeHeader('Content-Length');
+	//     this.removeHeader('Transfer-Encoding');
+	//     chunk = '';
+	//   }
+
+	//   if (req.method === 'HEAD') {
+	//     // skip body for HEAD
+	//     this.end();
+	//   } else {
+	//     // respond
+	//     this.end(chunk, encoding);
+	//   }
 };
-
-// res.send = function send(body) {
-//   var chunk = body;
-//   var encoding;
-//   var len;
-//   var req = this.req;
-//   var type;
-
-//   switch (typeof chunk) {
-//     // string defaulting to html
-//     case 'string':
-//       if (!this.get('Content-Type')) {
-//         this.type('html');
-//       }
-//       break;
-//     case 'boolean':
-//     case 'number':
-//     case 'object':
-//       if (chunk === null) {
-//         chunk = '';
-//       } else if (Buffer.isBuffer(chunk)) {
-//         if (!this.get('Content-Type')) {
-//           this.type('bin');
-//         }
-//       } else {
-//         return this.json(chunk);
-//       }
-//       break;
-//   }
-
-//   // write strings in utf-8
-//   if (typeof chunk === 'string') {
-//     encoding = 'utf8';
-//     type = this.get('Content-Type');
-
-//     // reflect this in content-type
-//     if (typeof type === 'string') {
-//       this.set('Content-Type', setCharset(type, 'utf-8'));
-//     }
-//   }
-
-//   // populate Content-Length
-//   if (chunk !== undefined) {
-//     if (!Buffer.isBuffer(chunk)) {
-//       // convert chunk to Buffer; saves later double conversions
-//       chunk = new Buffer(chunk, encoding);
-//       encoding = undefined;
-//     }
-
-//     len = chunk.length;
-//     this.set('Content-Length', len);
-//   }
-
-//   // freshness
-//   if (req.fresh) this.statusCode = 304;
-
-//   // strip irrelevant headers
-//   if (204 == this.statusCode || 304 == this.statusCode) {
-//     this.removeHeader('Content-Type');
-//     this.removeHeader('Content-Length');
-//     this.removeHeader('Transfer-Encoding');
-//     chunk = '';
-//   }
-
-//   if (req.method === 'HEAD') {
-//     // skip body for HEAD
-//     this.end();
-//   } else {
-//     // respond
-//     this.end(chunk, encoding);
-//   }
-
-//   return this;
-// };
