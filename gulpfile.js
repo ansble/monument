b99583810271ae8a84b086376dfeefb57ed6c44b
Bruce Campbell <campbellbd@ldschurch.org>
2015-03-20 17:06:07 -0600

    modify gulp build to roll patch and minor versions when appropriate

--- gulpfile.js
+++ gulpfile.js
@@ -19,8 +19,11 @@ var gulp = require('gulp')
 
 		if(type === 'major'){
 			versionArr[0] = parseInt(versionArr[0], 10) + 1;
+			versionArr[1] = 0;
+			versionArr[2] = 0;
 		} else if (type === 'minor') {
 			versionArr[1] = parseInt(versionArr[1], 10) + 1;
+			versionArr[2] = 0;
 		} else {
 			versionArr[2] = parseInt(versionArr[2], 10) + 1;
 		}
@@ -86,3 +89,8 @@ gulp.task('release', ['test'], function(){
 		});
 	});
 });
+
+// so we can test the gulpfile
+if (typeof exports !== 'undefined') {
+   exports.incrementVersion = incrementVersion;
+}
