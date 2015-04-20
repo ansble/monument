23d81ec86f15bc6879851f75b547843171a05e1b
Daniel Sellers <daniel@designfrontier.net>
2015-04-20 16:24:00 -0700

    added a cleanup file and hooked it up in utils. It will contain all the tasks to be executed at shutdown of the server by default. The exposed task in the utils folder just executes all of the functions exposed by the cleanup module in sequence

--- utils/utils.js
+++ utils/utils.js
@@ -1,7 +1,20 @@
 var getCompression = require('./getCompression')
-  , send = require('./send');
+  , send = require('./send')
+  , cleanup = require('./cleanup');
 
 module.exports = {
   getCompression: getCompression
   , send: send
+  , cleanup: function () {
+  		'use strict';
+
+  		//execute all of the cleanup tasks in the cleanup obejct
+  		//	not functionally pure... has sideeffects in the file system
+  		//	sorry world
+  		Object.keys(cleanup).forEach(function (key) {
+  			if(typeof cleanup[key] === 'function'){
+  				cleanup[key]();
+  			}
+  		});
+  }
 };
