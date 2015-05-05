2860841e1dc76d4e4a879af475a5e94204a1ac3c
Daniel Sellers <daniel@designfrontier.net>
2015-05-05 08:11:09 -0600

    more tests for the emitter. Most of .on is now tested

--- emitter_test.js
+++ emitter_test.js
@@ -12,11 +12,129 @@ describe('Event Tests', function(){
 
     afterEach(function () {
         emitter.off('some-event');
+        emitter.off('some-other-event');
     });
 
-	it('should have  an "on" function', function(){
-		assert.isFunction(emitter.on);
-	});
+    describe('.on tests', function () {
+    	it('should have  an "on" function', function(){
+    		assert.isFunction(emitter.on);
+    	});
+
+        it('should execute the callback passed to "on" when an event is triggered', function(done){
+            emitter.on('some-event', function () {
+                assert.strictEqual(true, true);
+                done();
+            });
+
+            emitter.emit('some-event');
+        });
+
+        it('should execute the callback passed to "on" when an event is triggered and recieve data', function (done){
+            emitter.on('some-event', function (data) {
+                assert.isObject(data);
+                done();
+            });
+
+            emitter.emit('some-event', {name: 'Tom Sawyer'});
+        });
+
+        it('should execute the callback passed to "on" with the scope passed in', function (done){
+            emitter.on('some-event', function (data) {
+                assert.strictEqual(data.name, this.name);
+                done();
+            }, {name: 'Tom Sawyer'});
+
+            emitter.emit('some-event', {name: 'Tom Sawyer'});
+        });
+
+        it('should execute the callback passed to "on" with the scope passed only once when flagged', function (done){
+            var count = 0;
+
+            emitter.on('some-event', function () {
+                count++;
+
+            }, {}, true);
+
+            emitter.on('some-other-event', function () {
+                assert.strictEqual(count, 1);
+                done();
+            });
+
+            emitter.emit('some-event');
+            emitter.emit('some-event');
+
+            emitter.emit('some-other-event');
+        });
+
+        it('should allow an object with named keys instead of function params', function (done) {
+            var count = 0;
+
+            emitter.on({
+                eventName: 'some-event'
+                , handler: function () {
+                    count++;
+
+                }
+                , scope: {}
+                , once: true
+            });
+
+            emitter.on('some-other-event', function () {
+                assert.strictEqual(count, 1);
+                done();
+            });
+
+            emitter.emit('some-event');
+            emitter.emit('some-event');
+
+            emitter.emit('some-other-event');
+        });
+
+        it('should allow an object with named keys instead of function params for just once and handler', function (done) {
+            var count = 0;
+
+            emitter.on({
+                eventName: 'some-event'
+                , handler: function () {
+                    count++;
+
+                }
+                , once: true
+            });
+
+            emitter.on('some-other-event', function () {
+                assert.strictEqual(count, 1);
+                done();
+            });
+
+            emitter.emit('some-event');
+            emitter.emit('some-event');
+
+            emitter.emit('some-other-event');
+        });
+
+        it('should allow an object with named keys instead of function params for just handler', function (done) {
+            var count = 0;
+
+            emitter.on({
+                eventName: 'some-event'
+                , handler: function () {
+                    count++;
+
+                }
+            });
+
+            emitter.on('some-other-event', function () {
+                assert.strictEqual(count, 2);
+                done();
+            });
+
+            emitter.emit('some-event');
+            emitter.emit('some-event');
+
+            emitter.emit('some-other-event');
+        });
+    });
 
 	it('should have  a "once" function', function(){
 		assert.isFunction(emitter.once);
