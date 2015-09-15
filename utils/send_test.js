70f3e3f1d0308e33ee89e15d1646854fcdc2ff4e
Daniel Sellers <daniel@designfrontier.net>
2015-09-14 20:54:40 -0600

    Finishes the base tests for the send functionality
    Now .send is covered 100% with tests. Bringing us ever closer to
    reasonable test coverage for the project.

--- utils/send_test.js
+++ utils/send_test.js
@@ -1,21 +1,30 @@
 var assert = require('chai').assert
     , send = require('./send')
+    , zlib = require('zlib')
+    , etag = require('etag')
 
     , fakeRes
     , fakeOut
     , fakeEncode
-    , obj;
+    , fakeHeaders
+    , obj
+    , buf;
 
 describe('Send Tests', function () {
     'use strict';
 
     beforeEach(function () {
+        fakeHeaders = {};
+
         fakeRes = {
-            setHeader: function () {}
+            setHeader: function (key, value) {
+                fakeHeaders[key] = value;
+            }
             , end: function (data, encode) {
                 fakeOut = data;
                 fakeEncode = encode;
-            }
+            },
+            statusCode: 200
         };
 
         fakeRes.send = send({
@@ -25,12 +34,35 @@ describe('Send Tests', function () {
             }
         }, {compression: false});
 
+        fakeRes.sendDeflate = send({
+            headers: {
+                'accept-encoding': 'deflate'
+                , 'if-none-match': ''
+            }
+        }, {compression: 'deflate'});
+
+        fakeRes.sendGzip = send({
+            headers: {
+                'accept-encoding': 'gzip'
+                , 'if-none-match': ''
+            }
+        }, {compression: 'gzip'});
+
         fakeOut = '';
 
         obj = {
             title: 'Tom Sawyer'
             , Author: 'Samuel Langhorne Clemens'
         };
+
+        buf = new Buffer('this is a buffer', 'utf8');
+
+        fakeRes.sendEtag = send({
+            headers: {
+                'accept-encoding': 'none'
+                , 'if-none-match': etag(JSON.stringify(obj))
+            }
+        }, {compression: false});
     });
 
     it('should be defined as a function', function () {
@@ -56,6 +88,11 @@ describe('Send Tests', function () {
         assert.strictEqual(fakeOut, JSON.stringify(obj));
     });
 
+    it('should handle a buffer', function () {
+        fakeRes.send(buf);
+        assert.strictEqual(fakeOut, 'this is a buffer');
+    });
+
     it('should handle an array', function () {
         fakeRes.send(['one', 'two']);
         assert.strictEqual(fakeOut, JSON.stringify(['one', 'two']));
@@ -69,5 +106,22 @@ describe('Send Tests', function () {
     it('should handle a bool', function () {
         fakeRes.send(true);
         assert.strictEqual(fakeOut, JSON.stringify(true));
+    });
+
+    it('should return deflate compressed results if deflate header is sent', function () {
+        fakeRes.sendDeflate(obj);
+        assert.strictEqual(JSON.stringify(fakeOut), JSON.stringify(zlib.deflateSync(JSON.stringify(obj))));
+        assert.strictEqual(fakeHeaders['Content-Encoding'], 'deflate');
+    });
+
+    it('should return gzip compressed results if gzip header is sent', function () {
+        fakeRes.sendGzip(obj);
+        assert.strictEqual(JSON.stringify(fakeOut), JSON.stringify(zlib.gzipSync(JSON.stringify(obj))));
+        assert.strictEqual(fakeHeaders['Content-Encoding'], 'gzip');
+    });
+
+    it('should return a 304 if the content has not changed', function () {
+        fakeRes.sendEtag(obj);
+        assert.strictEqual(fakeRes.statusCode, 304);
     });
 });
