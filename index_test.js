bd09e85a87204058b8054faade7bc5bbbd79e3a0
Daniel Sellers <daniel@designfrontier.net>
2015-09-16 08:15:57 -0600

    ES6 syntax for the main files and test
    yep

--- index_test.js
+++ index_test.js
@@ -1,29 +1,31 @@
-var assert = require('chai').assert
+'use strict';
+
+const assert = require('chai').assert
     , app = require('./index')
-    , http = require('http')
-    , server;
+    , http = require('http');
 
-describe('The main monument tests', function () {
-  'use strict';
+let server;
 
-    it('should be correctly defined', function () {
+describe('The main monument tests', () => {
+
+    it('should be correctly defined', () => {
         assert.isFunction(app.server);
         assert.isFunction(app.parser);
         assert.isObject(app.events);
     });
 
-    describe('Parser Tests', function () {
-        it('should have a parser method', function () {
+    describe('Parser Tests', () => {
+        it('should have a parser method', () => {
             assert.isFunction(app.parser);
         });
     });
 
-    describe('Wrapper Tests', function () {
-        afterEach(function () {
+    describe('Wrapper Tests', () => {
+        afterEach(() => {
           server.close();
         });
 
-        it('should return a server when run', function () {
+        it('should return a server when run', () => {
           server = app.server({
             routeJSONPath: './test_stubs/routes_stub.json'
             , templatePath: './test_stubs/templates'
@@ -34,12 +36,12 @@ describe('The main monument tests', function () {
         });
     });
 
-    describe('Compression Tests', function () {
+    describe('Compression Tests', () => {
         it('should compress things by default');
         it('should not compress things if compression is turned off');
     });
 
-    describe('doTjs Tests', function () {
+    describe('doTjs Tests', () => {
         it('should use doTjs defaults if none are set');
         it('should override doTjs specified defaults when set');
         it('should ignore irrelevant config keys');
