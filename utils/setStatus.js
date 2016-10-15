8694f6fdd532917cffc8dbe807a6c59032fbfa14
Rahul Jain <rahul_jain@live.in>
2016-10-15 07:26:05 +0530

    Changed function to reflect project coding style

--- utils/setStatus.js
+++ utils/setStatus.js
@@ -1,26 +1,27 @@
 'use strict';
 
 const statusCodes = require('http').STATUS_CODES
+    , isUndefined = require('./tools').isUndefined
     , setStatus = () => {
         return function (status) {
             /* eslint-disable no-invalid-this */
             const that = this;
             /* eslint-enable no-invalid-this */
-            let k;
 
-            if (status in statusCodes) {
+            if (!isUndefined(statusCodes[status])) {
                 that.statusCode = parseInt(status, 10);
                 that.statusMessage = statusCodes[status];
-                return;
+                return true;
             }
 
-            for (k in statusCodes) {
+            return Object.keys(statusCodes).some((k) => {
                 if (statusCodes[k].toLowerCase() === status.toLowerCase()) {
                     that.statusCode = parseInt(k, 10);
                     that.statusMessage = statusCodes[k];
-                    return;
+                    return true;
                 }
-            }
+                return false;
+            });
         };
     };
 
