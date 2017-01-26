ff0baeaa81ad66ae2cdd8ddfff3b24af27e2058e
Daniel Sellers <dsellers@instructure.com>
2017-01-25 18:33:56 -0700

    fixes a bad copy pasta job that should have been in a comment

--- utils/statsd.js
+++ utils/statsd.js
@@ -8,35 +8,3 @@ module.exports = {
         return new StatsD(options);
     }
 };
-
-
-
-    var startTime = new Date().getTime();
-
-    // Function called on response finish that sends stats to statsd
-    function sendStats() {
-      var key = req[options.requestKey];
-      key = key ? key + '.' : '';
-
-      // Status Code
-      var statusCode = res.statusCode || 'unknown_status';
-      client.increment(key + 'status_code.' + statusCode);
-
-      // Response Time
-      var duration = new Date().getTime() - startTime;
-      client.timing(key + 'response_time', duration);
-
-      cleanup();
-    }
-
-    // Function to clean up the listeners we've added
-    function cleanup() {
-      res.removeListener('finish', sendStats);
-      res.removeListener('error', cleanup);
-      res.removeListener('close', cleanup);
-    }
-
-    // Add response listeners
-    res.once('finish', sendStats);
-    res.once('error', cleanup);
-    res.once('close', cleanup);
