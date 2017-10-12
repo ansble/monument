350b630a3f34a87f78cabc7666169f293d06d5a0
Daniel Sellers <daniel@designfrontier.net>
2017-10-12 08:27:59 -0600

    refactored out some private functions for easy testing

--- web-sockets/socketHandler.js
+++ web-sockets/socketHandler.js
@@ -1,38 +1,22 @@
 'use strict';
 const events = require('harken')
       , isUndefined = require('../utils').isUndefined
-
-      , isDataEvent = (event, setEvent) => {
-        return event !== setEvent;
-      }
-
-      , getMessage = (dataIn) => {
-        try {
-          return JSON.parse(dataIn);
-        } catch (err) {
-          return {};
-        }
-      }
-      , getSetEventString = (message) => {
-        const shouldReplace = message.event && message.event.replace;
-
-        return shouldReplace ? message.event.replace(':get:', ':set:') : '';
-      };
+      , socketUtils = require('./utils');
 
 module.exports = (type) => {
   return (socketIn) => {
     const socket = socketIn;
 
     socket.onmessage = (messageIn) => {
-      const message = getMessage(messageIn.data)
-            , setEvent = getSetEventString(message);
+      const message = socketUtils.getMessage(messageIn.data)
+            , setEvent = socketUtils.getSetEventString(message);
 
       if (!type || isUndefined(message.event)) {
         // no event then we can't really do anything...
         return;
       }
 
-      if (type && type !== 'passthrough' && isDataEvent(message.event, setEvent)) {
+      if (type && type !== 'passthrough' && socketUtils.isDataEvent(message.event, setEvent)) {
         events.on(setEvent, (data) => {
           socket.send(JSON.stringify({ event: setEvent, data: data }), (err) => {
             if (err) {
