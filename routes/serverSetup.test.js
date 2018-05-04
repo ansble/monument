e388b6f3a499f32594f74b9615ff98437b87d21d
Daniel Sellers <daniel@designfrontier.net>
2018-05-04 08:20:19 -0600

    fixes broken test

--- routes/serverSetup.test.js
+++ routes/serverSetup.test.js
@@ -12,7 +12,9 @@ test('should return the list of public folders', (t) => {
 });
 
 test('should throw if an invalid value for routePath is passed', (t) => {
-  t.throws(setup, 'TypeError: path must be a string or Buffer', TypeError);
+    const invalidPathString = '(TypeError \[ERR_INVALID_ARG_TYPE\]: The "path" argument must be one of type string, Buffer, or URL. Received type undefined)' +
+      '| (TypeError: path must be a string or Buffer)';
+  t.throws(setup, new RegExp(invalidPathString), TypeError);
   t.throws(() => {
     setup('somewhere', 'else');
   }, 'Error: ENOENT: no such file or directory, scandir \'somewhere\'');
