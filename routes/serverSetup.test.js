afd157235bb6a812b2e19486b2382af0d3cfb293
Daniel Sellers <daniel@designfrontier.net>
2018-05-04 08:23:52 -0600

    adds in fix for broken test in node 10

--- routes/serverSetup.test.js
+++ routes/serverSetup.test.js
@@ -12,8 +12,9 @@ test('should return the list of public folders', (t) => {
 });
 
 test('should throw if an invalid value for routePath is passed', (t) => {
-    const invalidPathString = '(TypeError \[ERR_INVALID_ARG_TYPE\]: The "path" argument must be one of type string, Buffer, or URL. Received type undefined)' +
-      '| (TypeError: path must be a string or Buffer)';
+  const invalidPathString = '(TypeError \[ERR_INVALID_ARG_TYPE\]: The "path" argument must be one of type string, Buffer, or URL. Received type undefined)' +
+    '| (TypeError: path must be a string or Buffer)';
+
   t.throws(setup, new RegExp(invalidPathString), TypeError);
   t.throws(() => {
     setup('somewhere', 'else');
